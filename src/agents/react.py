d2269bc0361844bdcb585ba319c56cbc4c7813f2
Edmund Mills <edmund.martin.mills@gmail.com>
2023-12-13 14:11:32 -0600

    Initial commit

---
+++ src/agents/react.py
@@ -0,0 +1,70 @@
+import copy
+from typing import List, Optional
+
+from chatarena.backends import (BACKEND_REGISTRY, IntelligenceBackend,
+                                load_backend)
+from chatarena.config import BackendConfig
+from chatarena.message import SYSTEM_NAME, Message
+
+REASONING_PROMPT = "Before reponding to the previous message, first think step-by-step about your response. Give only your reasoning, not the response itself."
+
+def register_intelligence_backend(cls):
+    """Registers an intelligence backend with the backend registry."""
+    BACKEND_REGISTRY[cls.type_name] = cls
+    return cls
+
+
+@register_intelligence_backend
+class ReActWrapper(IntelligenceBackend):
+    type_name = 'react'
+    stateful = False
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)  # registers the arguments with Configurable
+        backend_kwargs = kwargs.get('backend', {})
+        backend_config = BackendConfig(**backend_kwargs)
+        self._backend = load_backend(backend_config)
+        self.message_pool = None
+
+    def get_reasoning(self, query_method, history_messages, request_msg, **kwargs):
+        """Returns the reasoning of a query method."""
+        reasoning_history = history_messages + [request_msg] if request_msg else history_messages
+        reasoning_request_msg = Message(SYSTEM_NAME, REASONING_PROMPT, history_messages[-1].turn)
+        return query_method(history_messages=reasoning_history, request_msg=reasoning_request_msg, **kwargs)
+
+
+    def get_action(self, query_method, history_messages, request_msg, agent_name, **kwargs):
+        """Returns the response of a query method."""
+        reasoning = self.get_reasoning(query_method, history_messages=history_messages, request_msg=request_msg, agent_name=agent_name, **kwargs)
+        reasoning = f'Thinking to myself: {reasoning} Next, when asked, I will give my response.'
+        reasoning_message = Message(agent_name, reasoning, history_messages[-1].turn, logged=True, visible_to=[])
+        if self.message_pool:
+            self.message_pool.append_message(reasoning_message)
+        if not request_msg:
+            request_msg = Message(SYSTEM_NAME, f"Now you speak, {agent_name}.", history_messages[-1].turn)
+        return query_method(history_messages=history_messages + [reasoning_message], request_msg=request_msg, agent_name=agent_name, **kwargs)
+
+
+    def query(
+        self,
+        agent_name: str,
+        role_desc: str,
+        history_messages: List[Message],
+        global_prompt: Optional[str] = None,
+        request_msg: Optional[Message] = None,
+        *args,
+        **kwargs,
+    ) -> str:
+        return self.get_action(self._backend.query, agent_name=agent_name, role_desc=role_desc, history_messages=history_messages, global_prompt=global_prompt, request_msg=request_msg, *args, **kwargs)
+
+    async def async_query(
+        self,
+        agent_name: str,
+        role_desc: str,
+        history_messages: List[Message],
+        global_prompt: Optional[str] = None,
+        request_msg: Optional[Message] = None,
+        *args,
+        **kwargs,
+    ) -> str:
+        return self.get_action(self._backend.async_query, agent_name=agent_name, role_desc=role_desc, history_messages=history_messages, global_prompt=global_prompt, request_msg=request_msg, *args, **kwargs)
---
+++ test.py
@@ -0,0 +1,30 @@
+import logging
+
+import chatarena
+from chatarena.arena import Arena
+from chatarena.backends.openai import OpenAIChat
+from src.agents.react import ReActWrapper
+
+logging.basicConfig(level=logging.INFO)
+
+def log_react_agent_reasoning(arena: Arena) -> None:
+    message_pool = arena.environment.message_pool
+    for player in arena.players:
+        if isinstance(player.backend, ReActWrapper):
+            player.backend.message_pool = message_pool
+
+
+def upgrade_to_gpt4(arena: Arena) -> None:
+    for player in arena.players:
+        backend = player.backend
+        if isinstance(backend, ReActWrapper):
+            backend = backend._backend
+        if isinstance(backend, OpenAIChat):
+            backend.model = 'gpt-4-1106-preview'
+
+if __name__ == '__main__':
+    arena = Arena.from_config('config.json')
+    log_react_agent_reasoning(arena)
+    # upgrade_to_gpt4(arena)
+    arena.run(num_steps=10)
+    arena.save_history('history.json')
